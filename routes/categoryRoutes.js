b093d2fcd997e53fb93c381ca5513bb3105be0a1
Jaimin Rana <ranajaimin33@gmail.com>
2024-12-24 23:33:45 +0530

    solve auth import issue

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/categoryController');
 const Cart = require('../models/cartModel');
+const auth = require('../middlewares/auth.middleware');
 
 
 router.get('/create', auth, categoryController.createCategory);
